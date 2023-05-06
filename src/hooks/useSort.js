8a3941d7cb3bcce02d15ca8cc53c0278b7905220
Zurab Zaridze <zaridzezuri@gmail.com>
2023-05-06 21:14:04 +0400

    Refactor sorting functionality 🔨

--- src/hooks/useSort.js
+++ src/hooks/useSort.js
@@ -1,60 +1,61 @@
 import { useState } from "react";
 
-import { sortByRank, sortByName, sortByPrice, sortBy1H, sortBy24H, sortBy7D, sortByMarketCap, sortByTotalSupply, sortByVolume } from "../functions/sortFunctions";
-
 export default function useSort(data) {
   const [sortedData, setSortedData] = useState(null);
   const [ascendingOrder, setAscendingOrder] = useState(true);
 
+  const sortBy = (key) => {
+    return (a, b) => {
+      let valueA = a;
+      let valueB = b;
+      const keys = key.split(".");
+      for (const k of keys) {
+        valueA = valueA[k];
+        valueB = valueB[k];
+      }
+      const diff = valueA - valueB;
+      return ascendingOrder ? diff : -diff;
+    };
+  };
+
   const handleSort = (event) => {
-    setAscendingOrder(true);
-
-    if (event == "rank") {
-      const sortedArr = sortByRank(data.data, ascendingOrder);
-      setSortedData({ data: sortedArr });
-    }
-
-    if (event == "name") {
-      const sortedArr = sortByName(data.data, ascendingOrder);
-      setSortedData({ data: sortedArr });
-    }
-
-    if (event == "price") {
-      const sortedArr = sortByPrice(data.data, ascendingOrder);
-      setSortedData({ data: sortedArr });
-    }
-
-    if (event == "1h") {
-      const sortedArr = sortBy1H(data.data, ascendingOrder);
-      setSortedData({ data: sortedArr });
-    }
-
-    if (event == "24h") {
-      const sortedArr = sortBy24H(data.data, ascendingOrder);
-      setSortedData({ data: sortedArr });
-    }
-
-    if (event == "7d") {
-      const sortedArr = sortBy7D(data.data, ascendingOrder);
-      setSortedData({ data: sortedArr });
-    }
-
-    if (event == "market_cap") {
-      const sortedArr = sortByMarketCap(data.data, ascendingOrder);
-      setSortedData({ data: sortedArr });
-    }
-
-    if (event == "volume") {
-      const sortedArr = sortByVolume(data.data, ascendingOrder);
-      setSortedData({ data: sortedArr });
-    }
-
-    if (event == "total_supply") {
-      const sortedArr = sortByTotalSupply(data.data, ascendingOrder);
-      setSortedData({ data: sortedArr });
-    }
-
     setAscendingOrder(!ascendingOrder);
+
+    let sortingFunction;
+    switch (event) {
+      case "rank":
+        sortingFunction = sortBy("cmc_rank");
+        break;
+      case "name":
+        sortingFunction = sortBy("name");
+        break;
+      case "price":
+        sortingFunction = sortBy("quote.USD.price");
+        break;
+      case "1h":
+        sortingFunction = sortBy("quote.USD.percent_change_1h");
+        break;
+      case "24h":
+        sortingFunction = sortBy("quote.USD.percent_change_24h");
+        break;
+      case "7d":
+        sortingFunction = sortBy("quote.USD.percent_change_7d");
+        break;
+      case "market_cap":
+        sortingFunction = sortBy("quote.USD.market_cap");
+        break;
+      case "volume":
+        sortingFunction = sortBy("quote.USD.volume_24h");
+        break;
+      case "total_supply":
+        sortingFunction = sortBy("total_supply");
+        break;
+      default:
+        return;
+    }
+
+    const sortedArr = data.data.slice().sort(sortingFunction);
+    setSortedData({ data: sortedArr });
   };
 
   return [sortedData, handleSort];
