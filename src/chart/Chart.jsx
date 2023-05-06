f43fc85318bf56163c991ebb5b7489ce4bba7138
Zurab Zaridze <zaridzezuri@gmail.com>
2023-05-07 00:11:21 +0400

    Add show rows functionality on UI to show only desired amount of rows

--- src/chart/Chart.jsx
+++ src/chart/Chart.jsx
@@ -6,12 +6,20 @@ import Body from "./Table/Body";
 
 import styles from "./Chart.module.css";
 
-import useSort from '../hooks/useSort'
+import useSort from "../hooks/useSort";
+import { useState } from "react";
 
 export default function Chart() {
-  const data = useIntervaledFetch(ENDPOINT_WITH_LIMIT(30), OPTIONS);
-  
+  const [showRowsSelect, setShowRowsSelect] = useState(false);
+  const [rows, setRows] = useState(100);
+
+  const data = useIntervaledFetch(ENDPOINT_WITH_LIMIT(rows), OPTIONS);
+
   const [sortedData, handleSort] = useSort(data);
+
+  const handleRowsSelect = () => {
+    setShowRowsSelect(!showRowsSelect);
+  };
 
   if (!data) {
     return <div>Loading...</div>;
@@ -19,6 +27,21 @@ export default function Chart() {
 
   return (
     <section className={styles.Section}>
+      <div className={styles.Rows}>
+        Show rows{" "}
+        <div className={styles.Button} onClick={handleRowsSelect}>
+          <div className="flex">{rows}<span className="ion-ios-arrow-down" /></div>
+
+          {showRowsSelect && (
+            <ul>
+              <li onClick={() => setRows(20)}>20</li>
+              <li onClick={() => setRows(50)}>50</li>
+              <li onClick={() => setRows(100)}>100</li>
+            </ul>
+          )}
+        </div>
+      </div>
+
       <table className={styles.Table}>
         <Head handleSort={handleSort} />
         <Body data={sortedData || data} />
